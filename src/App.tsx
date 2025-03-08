3ad56e79cc1f11f3854f815a6b572a360cbedd84
Aleydon <aleydonroberto@gmail.com>
2025-03-08 13:28:58 -0300

    ADD: Fetch games from api via axios library

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from 'react';
+
+import { api } from './services/api';
 import Logo from './assets/logo.svg';
 import GameBanner from './components/GameBanner';
 import CreateAdBanner from './components/CreateAdBanner';
 
+interface GameProps {
+	id: string;
+	title: string;
+	bannerUrl: string;
+	_count: {
+		ads: number;
+	};
+}
+
 export default function App() {
+	const [games, setGames] = useState<GameProps[]>([]);
+
+	useEffect(() => {
+		async function getGames() {
+			await api<GameProps[]>('http://localhost:3333/games').then(response => {
+				setGames(response.data);
+			});
+		}
+		void getGames();
+	}, []);
+
 	return (
 		<div className="flex flex-col items-center  max-w-[1344px] mx-auto my-20">
 			<img src={Logo} alt="Logo" />
@@ -15,24 +38,14 @@ export default function App() {
 			</h1>
 
 			<div className="grid grid-cols-6 gap-6 mt-16">
-				<GameBanner
-					bannerUrl="/game-1.png"
-					gameName="League of Legends"
-					adsCount={0}
-				/>
-				<GameBanner bannerUrl="/game-2.png" gameName="Dota 2" adsCount={1} />
-				<GameBanner bannerUrl="/game-3.png" gameName="CS: GO" adsCount={2} />
-				<GameBanner
-					bannerUrl="/game-4.png"
-					gameName="World of Warcraft"
-					adsCount={3}
-				/>
-				<GameBanner
-					bannerUrl="/game-5.png"
-					gameName="Apex Legends"
-					adsCount={4}
-				/>
-				<GameBanner bannerUrl="/game-6.png" gameName="Fortnite" adsCount={5} />
+				{games.map(game => (
+					<GameBanner
+						key={game.id}
+						bannerUrl={game.bannerUrl}
+						gameName={game.title}
+						adsCount={game._count.ads}
+					/>
+				))}
 			</div>
 
 			<CreateAdBanner />
