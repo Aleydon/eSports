872ae9e537652b295440b031273c449467104519
Aleydon <aleydonroberto@gmail.com>
2025-03-09 15:35:55 -0300

    ADD: Created mocks api to GameBanner Component

--- src/__tests__/App.spec.tsx
+++ src/__tests__/App.spec.tsx
@@ -1,6 +1,28 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import App from '@/App';
+
+const mockResponse = {
+	ok: true,
+	status: 200,
+	json: async () => [
+		{
+			bannerUrl: 'https://static-cdn.jtvnw.net/ttv-boxart/29595-285x380.jpg',
+			gameName: 'Dota 2',
+			adsCount: 0
+		},
+		{
+			bannerUrl:
+				'https://static-cdn.jtvnw.net/ttv-boxart/32982_IGDB-285x380.jpg',
+			gameName: 'Grand Theft Auto V',
+			adsCount: 0
+		}
+	]
+};
+
+global.fetch = jest.fn(
+	async () => await Promise.resolve(mockResponse as Response)
+);
 
 describe('App component', () => {
 	it('should render App Component', () => {
@@ -10,5 +32,17 @@ describe('App component', () => {
 	it('should render with svg logo', () => {
 		const icon = render(<App />);
 		expect(icon.container.querySelector('svg')).toBeInTheDocument();
+	});
+});
+
+describe('Component fetch data', () => {
+	it('should render GameBanner Component with response from api', async () => {
+		const app = render(<App />);
+		const dota = await screen.findByText(/Dota 2/i);
+		const gtaV = await screen.findByText(/Grand Theft Auto V/i);
+
+		expect(app.container).toBeInTheDocument();
+		expect(dota).toBeInTheDocument();
+		expect(gtaV).toBeInTheDocument();
 	});
 });
